a8d90057331a48f6761226832212b42260f13ee7
Hamza Zerouali <zeroualihamza0206@gmail.com>
2025-05-23 17:58:56 +0100

    doc: product description & installation guide documentation

--- enhanced_finder/main.py
+++ enhanced_finder/main.py
@@ -1,6 +1,10 @@
-"""Main entry point for Enhanced Finder."""
+"""Main entry point for Better Finder."""
 
-from .cli import main
+from .cli import cli_main
+
+def main():
+    """Entry point for better-finder command."""
+    cli_main()
 
 if __name__ == "__main__":
     main()
